3dea5d011631651f22dd46908c9984ce43ea02d2
Luis Parravicini <lparravi@gmail.com>
2011-11-04 11:18:32 -0300

    followers fetcher ready

--- zombie-followers.rb
+++ zombie-followers.rb
@@ -1,7 +1,7 @@
 require 'twitter'
 require 'fileutils'
 require 'logger'
-require 'time'
+require 'date'
 
 include FileUtils
 
@@ -12,8 +12,27 @@ $log.formatter = proc { |severity, datetime, progname, msg|
   "#{datetime}, #{severity}: #{msg}\n"
 }
 
+UPDATE_THRESHOLD = 5
 
-class ZombieDetector
+class TweetInfo
+  attr_reader :last_tweet, :updated_at
+
+  def initialize(last_tweet, prot=false)
+    @last_tweet = last_tweet
+    @updated_at = DateTime.now
+    @protected = prot
+  end
+
+  def protected?; @protected; end
+
+  def to_s
+    "tweet: %s, update: %s, protected=%s" % [@last_tweet, @updated_at, @protected]
+  end
+end
+
+module Zombie
+
+class Fetcher
   def initialize(usr, threshold)
     @threshold = threshold
     @usr = usr
@@ -38,17 +57,26 @@ class ZombieDetector
     i = 0
     @followers.keys.each do |uid|
       unless @followers[uid].nil?
-        next if (@followers[uid] - @threshold).abs < @threshold
+        next if @followers[uid].protected?
+        next if (now - @followers[uid].updated_at).abs < UPDATE_THRESHOLD
       end
 
-      result = Twitter.user_timeline(uid, :count => 1, :include_rts => 1,
-        :include_entities => 0, :trim_user => 1)
-      @followers[uid] = DateTime.parse(result.first['created_at'])
+      begin
+        result = Twitter.user_timeline(uid, :count => 1, :include_rts => 1,
+          :include_entities => 0, :trim_user => 1)
+        last_update = unless result.empty?
+          DateTime.parse(result.first['created_at'])
+        end 
+        @followers[uid] = TweetInfo.new(last_update)
 
-      $log.debug { "#{uid} tweeted on #{@followers[uid]}" }
+        $log.debug { "#{uid} tweeted on #{@followers[uid]}" }
+      rescue Twitter::Unauthorized
+        $log.debug { "#{uid} tweets are protected" }
+        @followers[uid] = TweetInfo.new(nil, true)
+      end
 
       i += 1
-      save_followers if i % 20 == 0
+      save_followers if i % 10 == 0
     end
 
     save_followers
@@ -62,7 +90,7 @@ class ZombieDetector
   end
 
   def save_followers
-    @log.debug { 'saving followers' }
+    $log.debug { 'saving followers' }
 
     tmp = @followers_db_name + '.tmp'
     File.open(tmp, 'w') { |io| Marshal.dump(@followers, io) }
@@ -96,9 +124,18 @@ class ZombieDetector
   def update_followers
     load_followers || fetch_followers
   end
-
 end
 
+class Reporter
+  def initialize(usr)
+    @usr = usr
+  end
+
+  def run
+  end
+end
+
+end
 
 
 usr = ARGV.shift
@@ -107,7 +144,10 @@ if usr.nil? || threshold < 1
   puts "usage: #{$0} <user> <threshold_in_days>"
   exit 1
 end
+if threshold < UPDATE_THRESHOLD
+  puts "threshold must be > #{UPDATE_THRESHOLD}"
+  exit 2
+end
 
-zombies = ZombieDetector.new(usr, threshold)
-
-zombies.run
+Zombie::Fetcher.new(usr, threshold).run
+Zombie::Reporter.new(usr).run
